b83d45ff3df287eab013a6f371a3f4bd7b1a9211
joannacollier <collj035@gmail.com>
2020-04-21 13:01:09 +0100

    add_entry method adds diary entry to an array

--- lib/secret_diary_editor.rb
+++ lib/secret_diary_editor.rb
@@ -1,2 +1,13 @@
 class SecretDiaryEditor
+
+  attr_accessor :entry 
+
+  def initialize 
+    @entry = []
+  end 
+
+  def add_entry(info)
+    @entry << info
+  end 
+      
 end 
--- spec/secret_diary_editor_spec.rb
+++ spec/secret_diary_editor_spec.rb
@@ -1,4 +1,11 @@
 require "./lib/secret_diary_editor"
 
 describe SecretDiaryEditor do 
+
+  describe "#add_entry" do 
+    it "adds user input into an array" do
+      subject.add_entry("test")
+      expect(subject.entry).to include("test")
+    end 
+  end
 end 
