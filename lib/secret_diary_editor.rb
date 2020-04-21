1f623d1c9c83f1bebb90d008b6d4b4963d4c0660
joannacollier <collj035@gmail.com>
2020-04-21 13:57:07 +0100

    Added a locked? method that checks if the diary is locked

--- lib/secret_diary_editor.rb
+++ lib/secret_diary_editor.rb
@@ -1,9 +1,12 @@
+require './lib/secret_diary_locked'
+
 class SecretDiaryEditor
 
-  attr_accessor :entry 
+  attr_accessor :entry, :locked
 
   def initialize 
     @entry = []
+    @locked = SecretDiaryLocked.new
   end 
 
   def add_entry(info)
@@ -11,10 +14,21 @@ class SecretDiaryEditor
   end 
 
   def get_entry(num)
-    index = num - 1
-    @entry.each do 
-      return @entry[index]
-    end 
+    if num == "all"
+      return @entry
+    else
+      @entry.each do |index|
+        index = num - 1
+        return @entry[index]
+      end 
+    end  
+  end 
+
+  def unlock_diary 
+    @locked.unlock
   end  
-      
+
+  def locked?
+    @locked.locked == true 
+  end 
 end 
--- spec/secret_diary_editor_spec.rb
+++ spec/secret_diary_editor_spec.rb
@@ -1,4 +1,5 @@
 require "./lib/secret_diary_editor"
+require './lib/secret_diary_locked'
 
 describe SecretDiaryEditor do 
 
@@ -13,6 +14,24 @@ describe SecretDiaryEditor do
     it "retrieves the entries stored in the Secret Diary" do 
       subject.add_entry("test")
       expect(subject.get_entry(1)).to eq("test")
+    end 
+
+    it "retrieves the entries stored in the Secret Diary" do 
+      subject.add_entry("test")
+      subject.add_entry("diary")
+      expect(subject.get_entry("all")).to eq(subject.entry)
+    end 
+  end 
+
+  describe "#unlock_diary" do 
+    it "changes SecretDiaryLocked from true to false" do 
+      expect(subject.unlock_diary).to eq(false)
+    end 
+  end 
+
+  describe "#locked?" do 
+    it "checks to see if diary is locked" do 
+      expect(subject.locked?).to eq(true)
     end 
   end 
 end 
